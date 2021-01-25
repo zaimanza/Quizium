b7bc45a32fa2e05f1a4596f76c559b8e39ed2488
MANZA <36519974+zaimanza@users.noreply.github.com>
2021-01-26 04:13:14 +0800

    f

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,7 +1,7 @@
 <?php
     // connect to database
-    $conn = mysqli_connect('localhost','root','','quizium');
-    // $conn = mysqli_connect('us-cdbr-east-03.cleardb.com','b310794f5353e9','d9f40fcf','heroku_f1cacb29cd6455f');
+    // $conn = mysqli_connect('localhost','root','','quizium');
+    $conn = mysqli_connect('us-cdbr-east-03.cleardb.com','b310794f5353e9','d9f40fcf','heroku_f1cacb29cd6455f');
     if(!$conn){
         echo 'Connection error' . mysqli_connect_error();
     } 
