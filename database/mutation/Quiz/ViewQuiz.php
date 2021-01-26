08bcfe785f6be6a4e0b0e49e7361dc8c975576d9
MANZA <36519974+zaimanza@users.noreply.github.com>
2021-01-26 11:13:14 +0800

    sdf

--- database/mutation/Quiz/ViewQuiz.php
+++ database/mutation/Quiz/ViewQuiz.php
@@ -1,6 +1,6 @@
 <?php
 $id = $_SESSION["instructorID"];
-$query = "SELECT * FROM quiz 
+$query = "SELECT * FROM quiz
 WHERE InstructorID = $id 
 ORDER BY quizID desc";
 $result1 = $conn->query($query);
